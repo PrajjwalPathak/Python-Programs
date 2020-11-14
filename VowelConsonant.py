48883d7f250b1f0e05e6f43310a124ce4f5c0dbc
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-11-14 12:48:07 +0530

    Add VowelConsonant.py

---
+++ VowelConsonant.py
@@ -0,0 +1,7 @@
+#Program to check if a character is a Vowel or a Consonant
+
+a=input("Enter the character to check if Vowel or Consonant: ")[0]
+if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u' or a=='A' or a=='E' or a=='I' or a=='O' or a=='U'):
+    print("Vowel")
+else:
+    print("Consonant")
